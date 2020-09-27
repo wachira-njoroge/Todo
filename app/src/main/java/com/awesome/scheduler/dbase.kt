83d2a7b62fd68336ce1de118a373aa44dfbfe899
Solo <solomonwachira212@gmail.com>
2020-09-27 17:15:33 +0300

    Created a class to handle communication to and from the server

---
+++ app/src/main/java/com/awesome/scheduler/dbase.kt
@@ -0,0 +1,17 @@
+package com.awesome.scheduler
+
+import java.util.*
+
+//This class is built to send requests to the server.
+//Requests sent can be a command to create  a record,
+//update a record or delete a record. In all this cases,
+//we need the data to work with...i.e., what to create, update or delete
+class dbase(val milk:String){
+
+    fun write(){
+        milk
+    }
+    fun update(){
+
+    }
+}
