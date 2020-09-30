f24315894316721e92fd07260a6deb9a5cc08cc7
Solo <solomonwachira212@gmail.com>
2020-09-30 20:27:48 +0300

    added access modifiers to my functions.

--- app/src/main/java/com/awesome/scheduler/MainActivity.kt
+++ app/src/main/java/com/awesome/scheduler/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.awesome.scheduler
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
@@ -24,14 +22,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //
-        //set the button clicklistener to this (since this class implements onclicklistener
+        //set the button ClickListener to this (since this class implements onclicklistener)
         save.setOnClickListener(this)
 
     }
-    //Once the user clicks the save button,
-    // get-> the selected option.
-    //       the schedule description.
-    //       the date.
+    //Once the user clicks the save button, get the information and save the record.
     override fun onClick(v: View?) {
         //
         //Perform checks on edittext.If empty, alert user to type in a description.
@@ -40,16 +35,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             //Get the user input fed.
             val input = get_data()
             //
-            //Invoke the save to database method which requires a JSonObject.
+            //Invoke the send to database method which requires a JSonObject.
             send(input)
         }else
-        {Toast.makeText(this, "Type something to proceed", Toast.LENGTH_SHORT).show()}
+        Toast.makeText(this, "Type something to proceed", Toast.LENGTH_SHORT).show()
     }
     //
     //Get the option selected and the description fed in by the user.
-    fun get_data(): JSONObject {
+    private fun get_data(): JSONObject {
         //
-        //Create a Json object structure where the data will be saved.
+        //Create a Json object structure where the data will be stored.
         val data = JSONObject()
         //
         //Get the user input.i.e the selected option, description and the date.
@@ -71,9 +66,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     //For that to happen I`ll need to make a post request.
     //that can be done by incorporating libraries such as retrofit or volley.
     //in this case I`ll work with volley to make the post request.
-    fun send(data:JSONObject){
+    private fun send(data:JSONObject){
         //
-        //Feed the server url..i.e where the data will be saved.
+        //Feed the server url..i.e where the data will be sent.
         val url = "http://mutall.co.ke/soloo/todo/todo.php"
         //
         //Create a requestque using the volley class.
@@ -82,8 +77,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //Define the nature of the request to the server...i.e is it a GET or a POST
         val jsonrequest = JsonObjectRequest(Request.Method.POST,url, data, {
             //
-            //With the response..i.e Onsuccess, Alert the and display the results.
-            response -> Toast.makeText(this, response.toString(), Toast.LENGTH_LONG ).show()
+            //OnSuccess, Alert and display the response.
+            response -> Toast.makeText(this, "$response", Toast.LENGTH_LONG ).show()
         }, {
             //
             //OnFailure, alert and display the error message.
