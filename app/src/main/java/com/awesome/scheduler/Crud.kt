1b4e71d0b797f975d914a3cc589b456f00bf0e4f
Solo <solomonwachira212@gmail.com>
2020-10-06 06:00:57 +0300

    Created A class to handle my scheduler operations

---
+++ app/src/main/java/com/awesome/scheduler/Crud.kt
@@ -0,0 +1,11 @@
+package com.awesome.scheduler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Crud:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.crud)
+    }
+}
